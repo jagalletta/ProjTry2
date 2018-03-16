be8719ac12f1569ae7f539dab084daad6966ac63
John Galletta <jagalletta@gmail.com>
2018-03-16 15:54:29 -0400

    Testing for publishing for public access.

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
-$to = 'abeerjm@bu.edu'; 
+$to = 'jag83@drexel.edu'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@musicbass.com\n"; 
--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
+
 // If session variable is not set it will redirect to login page
 if (!isset($_SESSION['userName']) || empty($_SESSION['userName'])) {
     header("location: login.php");
     exit;
 }
-
 ?>
 <!DOCTYPE html>
 <html>
