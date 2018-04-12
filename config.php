4de9b283443af2c878a270a85ab9060ff58f2076
John Galletta <jagalletta@gmail.com>
2018-04-12 11:20:22 -0400

    utf8 to link

--- config.php
+++ config.php
@@ -12,6 +12,7 @@ $dbName = "ProjectDB";
 
 /* Attempt to connect to MySQL database using object */
 $link = new mysqli($servername, $username, $password, $dbName);
+$link->set_charset('utf8');
 // Check connection
 if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
